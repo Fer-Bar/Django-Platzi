a834a6870c4a90742facc1eabbfd6956662c8e75
Fer-Bar <linofernando2703@gmail.com>
2022-05-18 22:42:33 -0400

    Change Function Based Views to Generic Views

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
         
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+    class Meta:
+        ordering = ["pub_date"]
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -19,3 +20,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text  
+    class Meta:
+        order_with_respect_to = "question"
